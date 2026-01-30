e141d201ebfbbdb8ff6be58212fec55965371324
animesh363 <animeshanand86033@gmail.com>
2026-01-30 22:22:35 +0530

    added universal .gitignore

---
+++ backend/src/routes/auth.route.js
@@ -0,0 +1,15 @@
+import express from "express"
+
+const router =express.Router();
+
+router.post("/signup",(req,res)=>{
+    res.send("signup routes");
+});
+router.post("/login",(req,res)=>{
+    res.send("login routes");
+});
+router.post("/logout",(req,res)=>{
+    res.send("logout routes");
+});
+
+export default router;
